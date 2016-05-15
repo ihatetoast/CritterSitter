dc848136edb8b18ac4a5a49795cb43493af1dbd1
Katy <ihatetoast@gmail.com>
2016-05-15 18:02:21 -0500

    added stock photos

--- public/scripts/components/Pages/Profile.js
+++ public/scripts/components/Pages/Profile.js
@@ -29,7 +29,7 @@ export default React.createClass({
 					<form onSubmit={this.makeProfile} >
 						<div className='photopara-sitters row'>
 							<div className='profileparadiv two-thirds column'>
-								<h5>Tell us about yourself:</h5>
+								<h5>Tell us about yourself, {this.state.user.get('firstName')}.</h5>
 								<div className='profilepara-div'>
 									<p className='profilepara'>Take a moment to let your personality shine. Are you 
 									active and taking your dogs on walks every day, or are you curled up on the couch
@@ -62,83 +62,101 @@ export default React.createClass({
 								best suits their critters. </p>
 							</div>
 						</div>
-						<div className='radiobuttons row'>
-							<div className='hmStyleSize four columns' ref='hmStyleSize'>
-								<h5>Home:</h5>
-								<label>
-									<input 
-									checked = {this.state.user.get('hmStyleSize')==='an apartment, condo, or townhouse'}
-									onChange={this.editHmStyleSize}
-									type='radio' 
-									name='home' 
-									className='radio' 
-									value='an apartment, condo, or townhouse' />
-								Apartment, Condo, Townhouse
-								</label>
-								<label>
-									<input 
-									checked = {this.state.user.get('hmStyleSize')==='a small house'}
-									onChange={this.editHmStyleSize}
-									type='radio' 
-									name='home' 
-									className='radio' 
-									value='a small house' />
-								Small house
-								</label>
-								<label>
-									<input 
-									checked = {this.state.user.get('hmStyleSize')==='a large house'}
-									onChange={this.editHmStyleSize}
-									type='radio' 
-									name='home' 
-									className='radio' 
-									value='a large house' />
-								Large house
-								</label>
+						<div className='radiobuttons-home row'>
+							<div className='hmStyleSize' ref='hmStyleSize'>
+								<h5>Home and yard styles:</h5>
+								<div className='three columns'>
+									<img src='./../../../../images/apartment-building-1149751_640.jpg'/>
+									<label>
+										<input 
+										checked = {this.state.user.get('hmStyleSize')==='an apartment, condo, or townhouse'}
+										onChange={this.editHmStyleSize}
+										type='radio' 
+										name='home' 
+										className='radio' 
+										value='an apartment, condo, or townhouse' />
+									Apartment, Condo, Townhouse
+									</label>
+								</div>
+								<div className='three columns'>
+									<img src='./../../../../images/smallhome.jpg'/>
+									<label>
+										<input 
+										checked = {this.state.user.get('hmStyleSize')==='a small house'}
+										onChange={this.editHmStyleSize}
+										type='radio' 
+										name='home' 
+										className='radio' 
+										value='a small house' />
+									Small house
+									</label>
+								</div>
+								<div className='three columns'>
+								<img src='./../../../../images/castle-973157_640.jpg'/>
+									<label>
+										<input 
+										checked = {this.state.user.get('hmStyleSize')==='a large house'}
+										onChange={this.editHmStyleSize}
+										type='radio' 
+										name='home' 
+										className='radio' 
+										value='a large house' />
+									Large house
+									</label>
+								</div>
 							</div>
-
-							<div className='ydStyleSize four columns' ref='ydStyleSize'>
-								<h5>Yard:</h5>
-								<label>
-									<input 
-										checked = {this.state.user.get('ydStyleSize')==='no yard'} 
-										onChange={this.editYdStyleSize}
-										type='radio' 
-										name='yard' 
-										className='radio' 
-										value='no yard' />
-									No yard
-								</label>
-								<label>
-									<input 
-										checked = {this.state.user.get('ydStyleSize')==='a small courtyard'} 
-										onChange={this.editYdStyleSize} 
-										type='radio' 
-										name='yard' 
-										className='radio'
-										value='a small courtyard' />
-									Small courtyard
-								</label>
-								<label>
-									<input 
-										checked = {this.state.user.get('ydStyleSize')==='a small fenced yard'} 
-										onChange={this.editYdStyleSize} 
-										type='radio' 
-										name='yard' 
-										className='radio' 
-										value='a small fenced yard' />
-									Small fenced yard
-								</label>
-								<label>
-									<input 
-										checked = {this.state.user.get('ydStyleSize')==='a large fenced yard'} 
-										onChange={this.editYdStyleSize}
-										type='radio' 
-										name='yard' 
-										className='radio' 
-										value='a large fenced yard' />
-									Large fenced yard
-								</label>
+						</div>
+						<div className='radiobuttons-yard row'>
+							<div className='ydStyleSize' ref='ydStyleSize'>
+								<div className='two columns'>
+									<label>
+										<input 
+											checked = {this.state.user.get('ydStyleSize')==='no yard'} 
+											onChange={this.editYdStyleSize}
+											type='radio' 
+											name='yard' 
+											className='radio' 
+											value='no yard' />
+										No yard
+									</label>
+								</div>
+								<div className='two columns'>
+									<label>
+										<input 
+											checked = {this.state.user.get('ydStyleSize')==='a small courtyard'} 
+											onChange={this.editYdStyleSize} 
+											type='radio' 
+											name='yard' 
+											className='radio'
+											value='a small courtyard' />
+										Small courtyard
+									</label>
+								</div>
+								<div className='offset-by-one two columns'>
+									<label>
+										<input 
+											checked = {this.state.user.get('ydStyleSize')==='a small fenced yard'} 
+											onChange={this.editYdStyleSize} 
+											type='radio' 
+											name='yard' 
+											className='radio' 
+											value='a small fenced yard' />
+										Small fenced yard
+									</label>
+								</div>
+								<div className='offset-by-one two columns'>
+									<label>
+										<input 
+											checked = {this.state.user.get('ydStyleSize')==='a large fenced yard'} 
+											onChange={this.editYdStyleSize}
+											type='radio' 
+											name='yard' 
+											className='radio' 
+											value='a large fenced yard' />
+										Large fenced yard
+									</label>
+								</div>
+								<div className='offset-by-one two columns'>
 								<label>
 									<input 
 										checked = {this.state.user.get('ydStyleSize')==='an unfenced yard'} 
@@ -149,40 +167,51 @@ export default React.createClass({
 										value='an unfenced yard' />
 									Unfenced yard
 								</label>
+								</div>
 							</div>
-
-							<div className='devEnviron four  columns' ref='devEnviron'>
-								<h5>Area:</h5>
-								<label>
-									<input 
-										checked = {this.state.user.get('devEnviron')==='city'} 
+						</div>
+						<div className='radiobuttons-devEnviron row'>
+							<div ref='devEnviron'>
+							<h5>Area:</h5>
+								<div className='devEnviron three columns'>
+									<img src='./../../../../images/urban.jpg'/>
+									<label>
+										<input 
+											checked = {this.state.user.get('devEnviron')==='city'} 
+											onChange={this.editDevEnivron} 
+											type='radio' 
+											name='dev-env' 
+											className='radio' 
+											value='city' />
+										City
+									</label>
+								</div>
+								<div className='devEnviron three columns'>
+								<img src='./../../../../images/suburbs.jpg'/>
+									<label>
+										<input 
+										checked = {this.state.user.get('devEnviron')==='suburbs'} 
 										onChange={this.editDevEnivron} 
 										type='radio' 
 										name='dev-env' 
 										className='radio' 
-										value='city' />
-									City
-								</label>
-								<label>
-									<input 
-									checked = {this.state.user.get('devEnviron')==='suburbs'} 
-									onChange={this.editDevEnivron} 
-									type='radio' 
-									name='dev-env' 
-									className='radio' 
-									value='suburbs' />
-								Suburbs
-								</label>
-								<label>
-									<input 
-										checked = {this.state.user.get('devEnviron')==='country'} 
-										onChange={this.editDevEnivron} 
-										type='radio' 
-										name='dev-env' 
-										className='radio' 
-										value='country' />
-									Country
-								</label>
+										value='suburbs' />
+									Suburbs
+									</label>
+								</div>
+								<div className='devEnviron three columns'>
+								<img src='./../../../../images/italy-105598_640.jpg'/>
+									<label>
+										<input 
+											checked = {this.state.user.get('devEnviron')==='country'} 
+											onChange={this.editDevEnivron} 
+											type='radio' 
+											name='dev-env' 
+											className='radio' 
+											value='country' />
+										Country
+									</label>
+								</div>
 							</div>
 						</div>
 						
@@ -247,3 +276,16 @@ export default React.createClass({
 		this.state.user.set('ydStyleSize', e.target.value);
 	}
 });
+
+
+{/*<div className='yardphoto row'>
+							<div className='four columns'>
+								<img src='./../../../../images/balcony.jpg'/>
+							</div>
+							<div className='four columns'>
+								<img src='./../../../../images/yard1.jpg'/>
+							</div>
+							<div className='four columns'>
+								<img src='./../../../../images/nofence.jpg'/>
+							</div>
+						</div>*/}
